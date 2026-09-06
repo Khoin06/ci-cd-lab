7105ccaae995d822695b2280f0d299154c0e2204
Khoin06 <ngokhoinsl@gmail.com>
2026-09-06 09:26:38 +0000

    push docker image to ghcr

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
 def add(a, b):
     return a + b
+
+
+@app.route("/")
+def home():
+    return "CI/CD Lab is running!"
+
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
