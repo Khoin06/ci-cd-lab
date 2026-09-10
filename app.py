7b3c27750b169d7705184e6282d9801bf6ede7d9
Khoin06 <ngokhoinsl@gmail.com>
2026-09-10 04:35:08 +0000

    fix app syntax

--- app.py
+++ app.py
@@ -2,22 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 def add(a, b):
     return a + b
 
-
 @app.route("/")
 def home():
-    return "CI/CD Lab is running!"
-
+    return "CI/CD Webhook auto trigger is running!"
 
 @app.route("/health")
 def health():
     return "OK", 200
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-return "CI/CD Webhook auto trigger is running!"
